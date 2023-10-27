ef0beb3e5cb6d9bb1e26c43b8e5fbf145cbce55d
Natali25072023 <infkab2@gmail.com>
2023-10-27 19:54:33 +0300

    last-version

--- scripts/task-3.js
+++ scripts/task-3.js
@@ -1,16 +1,15 @@
 function filterArray(numbers, value) {
    let mas = [];
-    for (let i = 0; i < numbers.length; i+=1) {
-        //const element = array[index];
-        if (numbers[i] > value) {
-         res = mas.push(numbers[i]);
+    for (const  number of numbers) {
+       if (number > value) {
+            mas.push(number);
     }
     }
-    return res;
+    return mas;
 }
-// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
     
